25857e5fb9e042ab773e78f9193f3f60eb0a2e4e
Faro145 <60946696+Faro145@users.noreply.github.com>
2020-11-24 15:31:56 +0000

    Create app.py

---
+++ service_one/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    animal = requests.get(localhost:5000/animal)
+    noise = requests.post(localhost:5000/noise, data = animal.text)
+    render_template('index.html', animal=animal.text, noise="noise.text")
+    
+if __name__ == '__main__':
+    app.run(port=5000, host='0.0.0.0')
